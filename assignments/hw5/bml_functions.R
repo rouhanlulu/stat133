fe610a9c298901f68163baad2d14f6acacbc07e0
rouhanlulu <rouhan1995@berkeley.edu>
2015-03-15 19:23:40 -0700

    hw5

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -7,11 +7,10 @@
 ## Output : A matrix [m] with entries 0 (no cars) 1 (red cars) or 2 (blue cars)
 ## that stores the state of the system (i.e. location of red and blue cars)
 
-bml.init <- function(r, c, p){
-
-  
-   #return(m)
+bml.init <- function(r,c,p){
+  matrix(sample(c(0,1,2), r*c, prob = c(1-p, p/2, p/2), replace = T), nrow = r)
 }
+
 
 #### Function to move the system one step (east and north)
 ## Input : a matrix [m] of the same type as the output from bml.init()
@@ -21,16 +20,47 @@ bml.init <- function(r, c, p){
 ## NOTE : the function should move the red cars once and the blue cars once,
 ## you can write extra functions that do just a step north or just a step east.
 
-bml.step <- function(m){
-
-  
-   #return(list(m, grid.new))
+move_east <- function(m) {
+  if(ncol(m)==1) {
+    return(m)
+  }
+  blocked <- m[, c(2:ncol(m), 1)]!=0
+  red_cars <- m*(m==1)
+  m*(m!=1) + red_cars*blocked + (red_cars*!blocked)[,c(ncol(m), 1:(ncol(m)-1))] 
 }
 
+move_north <- function(m) {
+  if(nrow(m)==1) {
+    return(m)
+  }
+  blocked <- m[c(nrow(m), 1:(nrow(m)-1)),]!=0
+  blue_cars <- m*(m==2)
+  m*(m!=2) + blue_cars*blocked + (blue_cars*!blocked)[c(2:nrow(m), 1), ] 
+}
+
+bml.step <- function(m){
+  m.new <- move_north(move_east(m))
+  return(list(m.new, !all(m==m.new)))
+}
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
 bml.sim <- function(r, c, p){
-
+  m=bml.init(r,c,p)
+  gridlock = F
+  i= 0
+  max.iter = 10000
+  while (!gridlock){
+    output=bml.step(m)
+    m = output[[1]]
+    gridlock = !output[[2]]
+    i= i+1
+    if (i>=max.iter){
+      warning ("max iteration numbers reached")
+      return (max.iter)
+      break
+    }
+  }
+  return (i)
 }
--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -6,5 +6,70 @@
 #### when you write up your results.
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
+source('~/src/stat133/assignments/hw5/bml_functions.R')
 
+#averages and SDs of two different sized matirces with three different densities
+#10*10 Matirx p=(0.21, 0.31, 0.61)
 
+matrix1=mean(replicate(100,bml.sim(10,10,0.21)))
+matrix11=mean(replicate(100,bml.sim(10,10,0.31)))
+matrix111=mean(replicate(100,bml.sim(10,10,0.61)))
+
+smsd1 = sd (replicate(100,bml.sim(10,10,0.21)))
+smsd2 = sd (replicate(100,bml.sim(10,10,0.31)))
+smsd3 = sd (replicate(100,bml.sim(10,10,0.61)))
+
+#100*100 Matrix p=(0.21, 0.31, 0.61)
+
+matrix2= mean(replicate(100,bml.sim(100,100,0.21)))
+matrix22= mean(replicate(100,bml.sim(100,100,0.31)))
+matrix222= mean(replicate(100,bml.sim(100,100,0.61)))
+
+bmsd1 = sd(replicate(100,bml.sim(100,100,0.21)))
+bmsd2 = sd(replicate(100,bml.sim(100,100,0.31)))
+bmsd3 = sd(replicate(100,bml.sim(100,100,0.61)))
+#Plot
+bml.sim.m <- function(r, c, p){
+  m=bml.init(r,c,p)
+  gridlock = F
+  i = 0
+  max = 10000
+  while (!gridlock){
+    output=bml.step(m)
+    m = output[[1]]
+    gridlock = !output[[2]]
+    i= i+1
+    if (i>=max){
+      warning ("max iteration reached")
+      return (0)
+      break
+    }
+  }
+  return (m)
+}
+
+####initial image
+image(bml.init(10,10,0.6),col=c("white","red","blue"),main="init for 10*10, p=0.6")
+image(bml.init(100,100,0.6),col=c("white","red","blue"),main="init for 100*100, p=0.6")
+
+####gridlock image
+
+image(t(apply(bml.sim.m(10,10,0.6),2,rev)),col=c("white","red","blue"), 
+      main = "gridlock for 10*10, p=0.6")
+image(t(apply(bml.sim.m(100,100,0.6),2,rev)),col=c("white","red","blue"), 
+      main = "gridlock for 100*100, p=0.6")
+
+# for the "steps vs. densities" plot
+diff.p= seq(from = 0.2,to = 1,by = 0.1)
+
+z=apply(replicate(10,sapply(diff.p, function(p) bml.sim(10,10,p))),1,mean)
+plot(diff.p, z, ylim=c(0,10000),main="steps VS density for 10*10", 
+     xlab ="density p", ylab= "avg # of steps", abline(v=0.32, col="red"))
+
+y=apply(replicate(10,sapply(diff.p, function(p) bml.sim(50,50,p))),1,mean)
+plot(diff.p, y, ylim=c(0,10000),main="steps VS density for 50*50", 
+     xlab ="density p", ylab= "avg # of steps", abline(v=0.32, col="red"))
+
+x=apply(replicate(10,sapply(diff.p, function(p) bml.sim(100,100,p))), 1, mean)
+plot(diff.p, x, ylim=c(0,10000),main="steps VS density for 100*100", 
+     xlab ="density p", ylab= "avg # of steps", abline(v=0.32, col="red"))
