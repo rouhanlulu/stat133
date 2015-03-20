22827290f59d25a3057c7347d143908395002aec
rouhanlulu <rouhan1995@berkeley.edu>
2015-03-20 14:36:49 -0700

    hw6

--- hw6/hw6.r
+++ hw6/hw6.r
@@ -38,9 +38,28 @@ sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
   # 3) convert the non-adopter with probability p
 
   # return the output
+  size=n.doctors*n.days
+  has_adopted=matrix(rep(0,size),nrow=n.doctors,ncol=n.days,byrow=FALSE)
+  has_adopted[,1]=t(t(initial.doctors))
 
+  update.doctors=initial.doctors
+
+  for (i in 2:n.days){
+    a=sample(1:n.doctors,size=2,replace=FALSE)
+    A1=a[1]
+    A2=a[2]
+    if (update.doctors[A1] == 1 & update.doctors[A2] == 0){
+      update.doctors[A2] <- sample(c(1,0), 1, prob=c(p,1-p))
+      has_adopted[,i] <- t(t(update.doctors))
+    } else if (update.doctors[A1] == 0 & update.doctors[A2]  == 1){
+      update.doctors[A1] <- sample(c(1,0), 1, prob=c(p,1-p))
+      has_adopted[,i] <- t(t(update.doctors))
+    } else{
+      has_adopted[,i] <- t(t(update.doctors))
+    }
+  }
+  return (has_adopted)
 }
-
 # When you test your function you have to generate <initial.doctors> and
 # pick values for the other input parameters.
 
@@ -50,4 +69,19 @@ set.seed(42)
 # on x-axis: days,
 # on y-axis : the number of doctors that have already adopted the drug, on that day
 # Put all 5 lines in one figure (e.g. use first plot() then lines() for the subsequent lines)
+initial.doctors <- sample(c(1,0), 100, replace=T, prob= c(0.1, 0.9))
+p1 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.11)
+plot(1:ncol(trial1), colSums(trial1),col="red",type="l", xlab="Days", ylab="Number of Doctor's Adoption on the Drug",
+     main="Stimulation of Drug Adoption",xlim=c(0,2500), ylim=c(0,100), las=1)
 
+p2 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.24)
+lines(1:ncol(trial2), colSums(trial2),col="green")
+
+p3 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.36)
+lines(1:ncol(trial3), colSums(trial3), col="yellow")
+
+p4 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.47)
+lines(1:ncol(trial4), colSums(trial4), col="pink")
+
+p5 <- sim.doctors(initial.doctors, n.doctors = 100, n.days=2500, p=0.53)
+lines(1:ncol(trial5), colSums(trial5), col="blue")
